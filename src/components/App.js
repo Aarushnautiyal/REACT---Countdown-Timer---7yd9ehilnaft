2dcac4b8aa973c98ab001e5fb18c8527b1af8c12
priyachimurkar <42578749+priyachimurkar1@users.noreply.github.com>
2022-01-20 20:41:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
-export default function App(){
+export default function App() {
   const [currentTime, setCurrentTime] = useState(0);
-  
-  const reverseCount =(event,inputValue) =>{
-  if(event.keyCode !==13) return;
-    inputValue =Math.floor(inputValue);
-    if(inputValue < 0){
-      inputValue =0;
-  }
+
+  const reverseCount = (event, inputValue) => {
+    if (event.keyCode !== 13) return;
+    inputValue = Math.floor(inputValue);
+    if (inputValue < 0) {
+      inputValue = 0;
+    }
+
     setCurrentTime(inputValue);
   };
+
   useEffect(() => {
     let id = null;
     if (currentTime > 0) {
@@ -20,22 +22,20 @@ export default function App(){
     return () => clearInterval(id);
   });
 
-
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
           Reverse countdown for
-                  <input 
-                      id="timeCount" 
-                       type="number"
-                        onKeyDown={(e) => reverseCount(e,e.target.value)}  
-                         />{" "} 
-                         sec.
+          <input
+            id="timeCount"
+            type="number"
+            onKeyDown={(e) => reverseCount(e, e.target.value)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{cureentTime/}</div>
+      <div id="current-time">{currentTime}</div>
     </div>
   );
 }
-
